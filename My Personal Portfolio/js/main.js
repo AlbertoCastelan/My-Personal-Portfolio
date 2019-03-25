7919d761d125212c4ca84a186b92d61225299359
AlbertoCastelan <albertovelacastelan@gmail.com>
2019-03-25 15:28:01 +0000

    Add files via upload

---
+++ My Personal Portfolio/js/main.js
@@ -0,0 +1,13 @@
+// CREATE A RESPONSIVE SCROLLDOWN NAVIGATION BAR
+var bars = document.getElementById('bars');
+var times = document.getElementById('times');
+var navlist = document.getElementById('navlist');
+
+function toggleClass() {
+    navlist.classList.toggle('responsive');
+    bars.classList.toggle('hidden');
+    times.classList.toggle('hidden');
+}
+
+bars.addEventListener('click', toggleClass);
+times.addEventListener('click', toggleClass);
